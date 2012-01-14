c10b7cbd2b76493577053e0e6fba2acac742e203
wilkie <wilkie05@gmail.com>
2012-01-13 23:51:12 -0500

    Adding better titles.

--- blog.rb
+++ blog.rb
@@ -4,7 +4,13 @@ Bundler.require
 require_relative 'post'
 
 class Blog < Sinatra::Base
+  TITLE = "Wilkie"
+
   helpers do
+    def title
+      return TITLE if @title.nil?
+      "#{@title} - #{TITLE}"
+    end
   end
 
   get '/' do
