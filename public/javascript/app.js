4a6e039d186c46f7f2bca021115cb28f796ecbc1
Lindsey Bieda <hazardous.narayan@gmail.com>
2012-10-26 18:51:57 -0400

    Updated styles and javascript to deal with smaller resolutions.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -8,6 +8,30 @@ $(document).ready(function(){
 
     // if code blocks were found, bring in the prettifier ...
     if ( prettify ) {
-    $.getScript("/javascript/prettify.js", function() { prettyPrint() });
+      $.getScript("/javascript/prettify.js", function() { prettyPrint() });
     }
+
+    changeStyles();
 });
+
+$(window).resize(function() {
+  changeStyles();
+});
+
+function changeStyles(){
+  if($(window).width() < 720) {
+    $('article').css('float', 'none');
+    $('article').css('width', 'auto');
+    $('article').css('padding', '1em');
+    $('aside').css('float', 'none');
+    $('aside').css('width', 'auto');
+    $('aside').css('padding', '1em');   
+  } else {
+    $('article').css('float', 'right');
+    $('article').css('width', '65%');
+    $('article').css('padding', '0 1em 0 0');
+    $('aside').css('float', 'left');
+    $('aside').css('width', '30%');
+    $('aside').css('padding', '0 0 0 1em');      
+  }
+}
