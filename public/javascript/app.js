a03ba06425088fbf61d76f4cde01e75095b397b8
Lindsey Bieda <hazardous.narayan@gmail.com>
2012-12-07 14:20:27 -0500

    Improvements for a more responsive design.
    - Fixed the title text being way to big on small screens
    - Aligned the header to be in the center when the screen is small
    - TODO: remove the style changes from the javascript and move it into CSS queries

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -18,6 +18,9 @@ $(window).resize(function() {
   changeStyles();
 });
 
+$("h1").fitText();
+
+// TODO: move into media query CSS
 function changeStyles(){
   if($(window).width() < 720) {
     $('article').css('float', 'none');
@@ -25,13 +28,23 @@ function changeStyles(){
     $('article').css('padding', '1em');
     $('aside').css('float', 'none');
     $('aside').css('width', 'auto');
-    $('aside').css('padding', '1em');   
+    $('aside').css('padding', '1em');
+    $('#title').css('display', 'block');
+    $('#title').css('text-align', 'center');
+    $('#title').css('float', 'none'); 
+    $('nav').css('text-align', 'center');
+    $('nav').css('float', 'none');    
   } else {
     $('article').css('float', 'right');
     $('article').css('width', '65%');
     $('article').css('padding', '0 1em 0 0');
     $('aside').css('float', 'left');
     $('aside').css('width', '30%');
-    $('aside').css('padding', '0 0 0 1em');      
+    $('aside').css('padding', '0 0 0 1em');
+    $('#title').css('display', 'inline');
+    $('#title').css('text-align', 'left'); 
+    $('#title').css('float', 'left');
+    $('nav').css('text-align', 'left');
+    $('nav').css('float', 'right');        
   }
 }
