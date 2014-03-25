9e1ae74ee6e35901c450e437ee2b7c47378c8193
Lindsey Bieda <hazardous.narayan@gmail.com>
2014-03-25 12:29:17 -0400

    Update app.js
    Stopped paying for gauges so remove this code.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,14 +15,3 @@ $(document).ready(function(){
 
 $("h1").fitText(0.8, { minFontSize: '20px', maxFontSize: '100px' });
 
-var _gauges = _gauges || [];
-(function() {
-  var t   = document.createElement('script');
-  t.type  = 'text/javascript';
-  t.async = true;
-  t.id    = 'gauges-tracker';
-  t.setAttribute('data-site-id', '511ea2a0108d7b5f2e000057');
-  t.src = '//secure.gaug.es/track.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(t, s);
-})();
