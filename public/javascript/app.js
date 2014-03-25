62a0acb998a325e54925dc6332d02bf2e25642a2
Lindsey Bieda <hazardous.narayan@gmail.com>
2014-03-25 12:37:46 -0400

    Update app.js
    Adding back in google analytics.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,3 +15,11 @@ $(document).ready(function(){
 
 $("h1").fitText(0.8, { minFontSize: '20px', maxFontSize: '100px' });
 
+// google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-4723762-1', 'rarlindseysmash.com');
+ga('send', 'pageview');
