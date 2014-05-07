74e40a552e76a39ccd011bef49975774e3c718f1
Lindsey Bieda <hazardous.narayan@gmail.com>
2014-05-06 21:16:57 -0400

    I really hate google analytics

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,11 +15,15 @@ $(document).ready(function(){
 
 $("h1").fitText(0.8, { minFontSize: '20px', maxFontSize: '100px' });
 
-// google analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-4723762-1', 'rarlindseysmash.com');
-ga('send', 'pageview');
+// gauges
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '511ea2a0108d7b5f2e000057');
+  t.src = '//secure.gaug.es/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
