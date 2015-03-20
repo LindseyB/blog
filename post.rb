f8515c6e804c12ce361c908c91af6707af91238a
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-03-20 12:40:26 -0400

    Now with support for tables

--- post.rb
+++ post.rb
@@ -16,6 +16,9 @@ class Post
       "</p><p class='image'><img src='#{link}' title='#{title}' alt='#{alt_text}' /><br /><span class='caption'>#{alt_text}</span>"
     end
 
+    def table(header, body)
+      "<table class='pure-table pure-table-striped'><thead>#{header}</thead><tbody>#{body}</tbody></table>"
+    end
   end
 end
 
@@ -54,7 +57,7 @@ class Post
   def content
     @content ||= begin
       renderer = Post::Renderer.new(@slug)
-      r = Redcarpet::Markdown.new(renderer, :fenced_code_blocks => true)
+      r = Redcarpet::Markdown.new(renderer, :fenced_code_blocks => true, :tables => true)
       r.render(@content_raw)
     end
   end
