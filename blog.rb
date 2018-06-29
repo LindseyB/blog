58bca27d3a1a689b0bf98a50bad6ede1d9e088f3
Lindsey Bieda <lindseyb@github.com>
2018-06-28 21:30:55 -0400

    Add not-pepe and video-to-gif to projects

--- blog.rb
+++ blog.rb
@@ -89,7 +89,9 @@ class Blog < Sinatra::Base
       {name: "Ascii Ascii Revolution", link: "https://github.com/LindseyB/AsciiAsciiRevolution", image: "https://imgur.com/8KY9fln.png"},
       {name: "LED Hula Hoops", link: "https://rarlindseysmash.com/posts/diy-led-hula-hoop", image: "https://i.imgur.com/26yZsc8.gif"},
       {name: "Accelerometer Galaxy Scarf", link: "https://github.com/LindseyB/galaxy-scarf", image: "https://i.giphy.com/l2Sq2kyWwZj37locg.gif"},
-      {name: "MESSANGERbag", link: "https://github.com/LindseyB/messangerbag", image: "http://i.imgur.com/KGvdCl8.gif"}
+      {name: "MESSANGERbag", link: "https://github.com/LindseyB/messangerbag", image: "http://i.imgur.com/KGvdCl8.gif"},
+      {name: "videos to gif", link: "https://github.com/LindseyB/videos-to-gif", image: "https://lindseyb.github.io/GIFs/Hackers/hack-the-planet.gif"},
+      {name: "not pepe", link: "https://github.com/LindseyB/not-pepe", image: "https://camo.githubusercontent.com/56220548c467640593c3fd31e99dcf555992f4e3/68747470733a2f2f6173736574732d6175746f2e72626c2e6d732f62656364343937356632613763306235663733353233626132346564636238316164383461346331373065653936313737373237383861316465326335323134"}
     ]
 
     games = HTTParty.get("https://itch.io/api/1/#{ENV.fetch('ITCH_API_KEY')}/my-games")
