34b7ff60b5cd4577e06953d1eb71c196a52344f5
Lindsey B <hazardous.narayan@gmail.com>
2019-11-04 11:39:26 -0500

    YOLO

---
+++ projects.rb
@@ -0,0 +1,16 @@
+PROJECTS = [
+      {name: "Star Wars Gifs", link: "https://rarlindseysmash.com/posts/stupid-programmer-tricks-and-star-wars-gifs", image: "https://i.imgur.com/8PJevlr.gif"},
+      {name: "XOmBot", link: "https://github.com/LindseyB/XOmBot", image: "https://rarlindseysmash.com/images/projects/xombot.png"},
+      {name: "Pride, Prejudice, Dungeons, and Dragons", link: "https://github.com/LindseyB/pride-prejudice-dungeons-dragons", image: "https://pbs.twimg.com/media/CUzh7m9XAAAPjwH.png"},
+      {name: "Green Jellybean Love Stars", link: "https://www.youtube.com/watch?v=fEEAONl4qos", image: "https://i.imgur.com/aoYrnMu.png"},
+      {name: "Iron Trotter", link: "https://github.com/LindseyB/IronTrotter", image: "https://i.imgur.com/CiGUh56.png"},
+      {name: "Ascii Ascii Revolution", link: "https://github.com/LindseyB/AsciiAsciiRevolution", image: "https://imgur.com/8KY9fln.png"},
+      {name: "LED Hula Hoops", link: "https://rarlindseysmash.com/posts/diy-led-hula-hoop", image: "https://i.imgur.com/26yZsc8.gif"},
+      {name: "Accelerometer Galaxy Scarf", link: "https://github.com/LindseyB/galaxy-scarf", image: "https://i.giphy.com/l2Sq2kyWwZj37locg.gif"},
+      {name: "MESSANGERbag", link: "https://github.com/LindseyB/messangerbag", image: "http://i.imgur.com/KGvdCl8.gif"},
+      {name: "videos to gif", link: "https://github.com/LindseyB/videos-to-gif", image: "https://lindseyb.github.io/GIFs/Hackers/hack-the-planet.gif"},
+      {name: "not pepe", link: "https://github.com/LindseyB/not-pepe", image: "https://camo.githubusercontent.com/56220548c467640593c3fd31e99dcf555992f4e3/68747470733a2f2f6173736574732d6175746f2e72626c2e6d732f62656364343937356632613763306235663733353233626132346564636238316164383461346331373065653936313737373237383861316465326335323134"},
+      {name: "Magic Crystal", link: "https://github.com/LindseyB/magic-crystal", image: "https://user-images.githubusercontent.com/33750/52889920-36ea9a00-3150-11e9-95f0-5323143d6b6e.gif"},
+      {name: "Cutie Finder", link: "https://lindseyb.github.io/cutiefinder/", image: "https://i.imgur.com/CGZ2hPs.png"},
+      {name: "GitHub Contribution Graph Art Generator", link: "https://glitch.com/~github-contribution-graph-art-generator", image: "https://i.imgur.com/NfeepnH.png"}
+]
