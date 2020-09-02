8a922c8683abec2dbc9b9d7f0423362449b77d71
Lindsey Bieda <hazardous.narayan@gmail.com>
2020-09-01 20:21:13 -0400

    Remove jquery and bootstrap

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,28 +1,7 @@
-$(document).ready(function(){
-    // add prettyprint class to all <pre><code></code></pre> blocks
-    var prettify = false;
-    $("pre code").parent().each(function() {
-      $(this).addClass('prettyprint');
-      prettify = true;
-    });
-
-    // if code blocks were found, bring in the prettifier ...
-    if ( prettify ) {
-      $.getScript("/javascript/prettify.js", function() { prettyPrint() });
-    }
-});
-
-$(".grid").masonry({
+var grid = document.querySelector('.grid');
+var msnry = new Masonry( grid, {
   itemSelector: '.grid-item',
   columnWidth: 400
 });
 
-$(window).on("resize", function () {
-    width = parseInt($('#calc').css('width'));
-    scale = width/552;
-    $(".itch-wrapper iframe").css('transform-origin', '0 0')
-                             .css('transform', 'scale('+scale+')');
-    $(".itch-wrapper").css('height', (167*scale)+'px');
-}).resize();
-
-
+hljs.initHighlightingOnLoad();
